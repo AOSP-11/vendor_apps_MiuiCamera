f8e667e152ee3c0657817e6ccbb77d7cfdbb4aef
sbwml <984419930@qq.com>
2019-11-22 07:43:48 +0800

    apps: MiuiCamera by sagit - Android 10

---
+++ config.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),sagit))
+
+VENDOR_PATH := vendor/apps/MiuiCamera
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc) \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib,system/lib) \
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,system/lib64)
+
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+
+# MiuiCamera
+PRODUCT_PACKAGES += \
+    MiuiCamera
+
+endif
