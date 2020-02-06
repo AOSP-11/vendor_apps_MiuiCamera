6b9b36097e475d58bb21fdddadcb79409a508325
sbwml <984419930@qq.com>
2020-02-06 22:45:14 +0800

    Revert "Suport MiuiCamera and Libs Alexey 60fps"
    This reverts commit 077b4eda4eef827067ae8a7aaadaf8762148c875.

--- config.mk
+++ config.mk
@@ -19,9 +19,7 @@ VENDOR_PATH := vendor/apps/MiuiCamera
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc) \
     $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib,system/lib) \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,system/lib64) \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc) \
-    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/vendor/lib,$(TARGET_COPY_OUT_VENDOR)/lib)
+    $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,system/lib64)
 
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
